5eee1489975181dd6710ce3865a6dae981e1a69a
lukhfzi <lukhfzi@gmail.com>
2025-10-12 02:29:39 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: "/court-sponsors-hub/", // 👈 change this to your repo name
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
-
+export default defineConfig({
+  base: "/court-sponsors-hub/", // 👈 replace with your repo name
+  plugins: [react()],
+});
